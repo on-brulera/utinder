0dbf7f5ff5b9690bd5fab104d5ab22a7ca261d9e
on-brulera <luishenryramirezmontalvo746@gmail.com>
2024-01-26 09:34:04 -0500

    Initial route configurated

--- lib/presentation/network/home_screen.dart
+++ lib/presentation/network/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  static String name = 'home_screen_view';
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Placeholder();
   }
 }

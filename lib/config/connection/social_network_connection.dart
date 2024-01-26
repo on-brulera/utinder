82e9d1fb89de1bed8482bb8f29a2cecdbeb9bda0
on-brulera <luishenryramirezmontalvo746@gmail.com>
2024-01-26 09:13:37 -0500

    Configurated configuration layer

---
+++ lib/config/connection/social_network_connection.dart
@@ -0,0 +1,10 @@
+import 'package:dio/dio.dart';
+
+final socialNetworkAPI = Dio(
+  BaseOptions(
+    baseUrl: 'http://localhost:3000',
+    queryParameters: {      
+      'language': 'es-Mx',
+    }
+  )
+);
